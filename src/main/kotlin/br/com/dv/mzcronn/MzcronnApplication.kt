c58265739811e9bd6474b5bd3aab1a8b83ee2c02
Douglas Vieira <douglas16722@gmail.com>
2024-03-05 19:08:52 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/dv/mzcronn/MzcronnApplication.kt
@@ -0,0 +1,11 @@
+package br.com.dv.mzcronn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MzcronnApplication
+
+fun main(args: Array<String>) {
+    runApplication<MzcronnApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/dv/mzcronn/MzcronnApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.dv.mzcronn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MzcronnApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
