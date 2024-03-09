d6d1f703365b8393d9e5a1c0b725cfb95a64e6c1
Douglas Vieira <douglas16722@gmail.com>
2024-03-09 12:42:31 -0300

    Added @Table annotation to specify table name

--- src/main/kotlin/br/com/dv/mzcronn/mrequest/MRequest.kt
+++ src/main/kotlin/br/com/dv/mzcronn/mrequest/MRequest.kt
@@ -1,11 +1,9 @@
 package br.com.dv.mzcronn.mrequest
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
+@Table(name = "request")
 class MRequest {
 
     @Id
