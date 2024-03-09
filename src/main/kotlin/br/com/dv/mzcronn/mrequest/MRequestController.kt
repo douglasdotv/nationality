8c7c1ab7ee0461bfd200076f77dac706676bece8
Douglas Vieira <douglas16722@gmail.com>
2024-03-09 12:40:04 -0300

    Log incoming requests

--- src/main/kotlin/br/com/dv/mzcronn/mrequest/MRequestController.kt
+++ src/main/kotlin/br/com/dv/mzcronn/mrequest/MRequestController.kt
@@ -1,5 +1,6 @@
 package br.com.dv.mzcronn.mrequest
 
+import br.com.dv.mzcronn.util.logger
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -7,12 +8,15 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class MRequestController(private val service: MRequestService) {
 
+    private val log by logger()
+
     companion object {
         const val CONFIRMATION = "Data received"
     }
 
     @PostMapping("/receive")
     fun receiveData(@RequestBody data: String): String {
+        log.info("Received data: $data")
         service.saveRequest(data)
         return CONFIRMATION
     }
