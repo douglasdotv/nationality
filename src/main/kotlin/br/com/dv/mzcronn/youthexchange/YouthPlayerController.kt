df4451fb0c73af002918d9cc1620dff4abde8b8c
Douglas Vieira <douglas16722@gmail.com>
2024-05-05 18:00:48 -0300

    Fix LoggerFactory is not a Logback LoggerContext but Logback is on the classpath

--- src/main/kotlin/br/com/dv/mzcronn/youthexchange/YouthPlayerController.kt
+++ src/main/kotlin/br/com/dv/mzcronn/youthexchange/YouthPlayerController.kt
@@ -1,6 +1,7 @@
 package br.com.dv.mzcronn.youthexchange
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,6 +13,11 @@ class YouthPlayerController(private val azureTableStorageService: AzureTableServ
     fun saveYouthExchangeData(@RequestBody data: YouthExchange): ResponseEntity<String> {
         azureTableStorageService.insertOrUpdateEntity(data)
         return ResponseEntity.ok("Data saved successfully to Azure Table Storage")
+    }
+
+    @GetMapping("/health")
+    fun saveYouthExchangeData(): ResponseEntity<String> {
+        return ResponseEntity.ok("Hello")
     }
 
 }
